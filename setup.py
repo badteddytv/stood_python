630ea4143612c3e29d56c0aaf7b1032a0e1da439
Neil Dwyer <neildwyer1991@gmail.com>
2019-03-05 21:04:52 +0000

    trying something

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='stood',
-        version='0.6',
+        version='0.7',
         description='',
         url='http://github.com/badteddytv/stood_python',
         author='Neil Dwyer',
         author_email='neil@badteddy.tv',
-        packages=['stood'],
+        packages=find_packages(exclude=['contrib', 'docs', 'tests']),
         tests_require=['pytest'],
         zip_safe=False)
