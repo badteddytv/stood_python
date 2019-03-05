8b2e2b6cffd71bb40a7c8ee4ad5a5d032061ecbe
Neil Dwyer <neildwyer1991@gmail.com>
2019-03-05 20:54:03 +0000

    no longer requires aiohttp

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='stood',
-        version='0.4',
+        version='0.5',
         description='',
         url='http://github.com/badteddytv/stood_python',
         author='Neil Dwyer',
         author_email='neil@badteddy.tv',
         packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-        install_requires=[
-            'aiohttp'
-            ],
         tests_require=['pytest'],
         zip_safe=False)
