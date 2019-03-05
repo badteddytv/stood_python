0c48f9554ab300368547b50fbb7d1ea0f84c11e3
Neil Dwyer <neildwyer1991@gmail.com>
2019-03-05 21:07:22 +0000

    install required elasticsearch

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='stood',
-        version='0.7',
+        version='0.8',
         description='',
         url='http://github.com/badteddytv/stood_python',
         author='Neil Dwyer',
         author_email='neil@badteddy.tv',
         packages=find_packages(exclude=['contrib', 'docs', 'tests']),
         tests_require=['pytest'],
+        install_requires=['elasticsearch'],
         zip_safe=False)
