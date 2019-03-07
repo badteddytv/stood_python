f06f9c17c22593782b23824e40b3263f8fa6cd83
Neil Dwyer <neildwyer1991@gmail.com>
2019-03-07 03:17:34 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='stood',
-        version='1.0',
+        version='1.1',
         description='',
         url='http://github.com/badteddytv/stood_python',
         author='Neil Dwyer',
