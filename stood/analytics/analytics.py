7384b23521ff5d7a365ff902e9de53844393ba34
Neil Dwyer <neildwyer1991@gmail.com>
2019-05-27 21:21:43 +0000

    fixes to analytics

--- stood/analytics/analytics.py
+++ stood/analytics/analytics.py
@@ -12,7 +12,6 @@ import elasticsearch.helpers as helpers
 
 hostname = os.getenv('HOSTNAME', 'NO_HOSTNAME')
 
-  #"doc_type": "logs-*",
 MAPPINGS = """
 {
   "mappings": {
@@ -27,40 +26,21 @@ MAPPINGS = """
 }
 """
 
-#routing key and data (payload, user, etc.)
 
 class DummyEventTracker(object):
     def track_event(self, routing_key, data):
         pass
 
+
 def get_current_day():
     return datetime.datetime.today().strftime('%Y-%m-%d')
+
 
 class EventTracker(object):
     def __init__(self, name):
         self.es = None
         self.name = name
         self.queue = queue.Queue()
-        self.current_day = get_current_day()
-        setup_thread = Thread(target=self._setup)
-        setup_thread.start()
-
-    def _setup(self):
-        try:
-            self.es = Elasticsearch(
-                    [config.ELASTICSEARCH_URL],
-                    sniff_on_start=True,
-                    sniff_on_connectoin_fail=True,
-                    snifffer_timeout=30
-                    )
-            res = self.es.indices.create('analytics-{}'.format(self.current_day), ignore=400, body=MAPPINGS)
-            loop_thread = Thread(target=self._loop)
-            loop_thread.start()
-
-        except Exception:
-            traceback.print_exc()
-            time.sleep(15)
-            self._setup()
 
     def _loop(self):
         while True:
@@ -71,14 +51,12 @@ class EventTracker(object):
         self.queue.put((routing_key, data))
 
     def _send_events(self):
+
         new_day = get_current_day()
-        if self.current_day != new_day:
-            try:
-                self.current_day = new_day
-                self.es.indices.create('log-{}'.format(self.current_day), ignore=400, body=MAPPINGS)
-            except Exception:
-                traceback.print_exc()
-                return
+        index = 'analytics-{}'.format(new_day)
+
+        if not self.es.indices.exists(index=index):
+            self.es.indices.create(index, ignore=400, body=MAPPINGS)
 
         current_time = datetime.datetime.now().timestamp()
         index = 'analytics-{}'.format(self.current_day)
@@ -96,7 +74,7 @@ class EventTracker(object):
                 'timestamp': int(current_time * 1000),
                 'service': self.name,
                 'routing_key': routing_key,
-                'data':data,
+                'data': data,
                 'hostname': hostname
                 })
             try:
@@ -110,7 +88,9 @@ class EventTracker(object):
 
         helpers.bulk(self.es, payload)
 
+
 logger = None
+
 
 def get_event_tracker(name):
     if config.ELASTICSEARCH_ENABLED:
@@ -118,9 +98,10 @@ def get_event_tracker(name):
 
     return DummyEventTracker()
 
+
 if __name__ == '__main__':
-    test_data = {'data':'data'}
+    test_data = {'data': 'data'}
     l = get_event_tracker(name='analytics')
-    l.track_event(routing_key='test.test',data = test_data)
+    l.track_event(routing_key='test.test', data=test_data)
     while True:
         pass
