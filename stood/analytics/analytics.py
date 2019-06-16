e5b20f9d31c35e961863cb143941ef404b99ce39
Neil Dwyer <neildwyer1991@gmail.com>
2019-06-16 18:01:57 +0000

    switch to file logger

--- stood/analytics/analytics.py
+++ stood/analytics/analytics.py
@@ -1,129 +1,70 @@
 import os
-import queue
 import sys
-import time
 import uuid
 import datetime
 import traceback
-from threading import Thread
+import json
 from stood.config import config
-from elasticsearch import Elasticsearch
-import elasticsearch.helpers as helpers
+import logging
+from logging import StreamHandler, Formatter
+from logging.handlers import RotatingFileHandler
+import time
 
 hostname = os.getenv('HOSTNAME', 'NO_HOSTNAME')
 
-MAPPINGS = """
-{
-  "mappings": {
-    "_doc": {
-      "properties": {
-        "timestamp": {
-          "type": "date"
-        }
-      }
-    }
-  }
-}
-"""
+
+logger = None
 
 
-class DummyEventTracker(object):
+def get_logger(name):
+    global logger
+
+    if logger is not None:
+        return logger
+
+    logger = logging.getLogger('{}-analytics'.format(name))
+    logger.setLevel(config.LEVEL)
+
+    elasticsearch_handler = RotatingFileHandler(
+            '/var/log/stood/{}.analytics'.format(name), maxBytes=1024 * 100, backupCount=10)
+    logger.addHandler(elasticsearch_handler)
+
+    return logger
+
+
+class EventTracker(object):
+    def __init__(self, name):
+        self.name = name
+        self.logger = get_logger(name)
+
     def track_event(self, routing_key, data):
-        pass
+        new_day = get_current_day()
+        index = 'log-{}'.format(new_day)
+
+        es_record = {
+                '_index': index,
+                '_type': '_doc',
+                '_id': str(uuid.uuid4()),
+                'timestamp': int(time.time() * 1000),
+                'service': self.name,
+                'routing_key': routing_key,
+                'data': data,
+                'hostname': hostname
+                }
+
+        self.logger.info(json.dumps(es_record))
 
 
 def get_current_day():
     return datetime.datetime.today().strftime('%Y-%m-%d')
 
 
-class EventTracker(object):
-    def __init__(self, name):
-        self.es = None
-        self.name = name
-        self.queue = queue.Queue()
-        setup_thread = Thread(target=self.setup)
-        setup_thread.start()
-
-    def setup(self):
-        try:
-            self.es = Elasticsearch(
-                    [config.ELASTICSEARCH_URL],
-                    sniff_on_start=True,
-                    sniff_on_connectoin_fail=True,
-                    snifffer_timeout=30
-                    )
-            loop_thread = Thread(target=self._loop)
-            loop_thread.start()
-        except Exception:
-            traceback.print_exc()
-            time.sleep(15)
-            self.setup()
-
-    def _loop(self):
-        while True:
-            time.sleep(10)
-            self._send_events()
-
-    def track_event(self, routing_key, data):
-        self.queue.put((routing_key, data))
-
-    def _send_events(self):
-
-        try:
-            new_day = get_current_day()
-            index = 'analytics-{}'.format(new_day)
-
-            if not self.es.indices.exists(index=index):
-                self.es.indices.create(index, ignore=400, body=MAPPINGS)
-
-            current_time = datetime.datetime.now().timestamp()
-        except Exception:
-            traceback.print_exc()
-            return
-
-        payload = []
-
-        try:
-            routing_key, data = self.queue.get_nowait()
-        except queue.Empty:
-            routing_key = None
-            data = None
-        while routing_key is not None:
-            payload.append({
-                '_index': index,
-                '_type': '_doc',
-                '_id': uuid.uuid4(),
-                'timestamp': int(current_time * 1000),
-                'service': self.name,
-                'routing_key': routing_key,
-                'data': data,
-                'hostname': hostname
-                })
-            try:
-                routing_key = self.queue.get_nowait()
-            except queue.Empty:
-                routing_key = None
-
-        if self.es is None:
-            print('ERROR: dropped logs')
-            return
-
-        helpers.bulk(self.es, payload)
-
-
-logger = None
-
-
 def get_event_tracker(name):
-    if config.ELASTICSEARCH_ENABLED:
-        return EventTracker(name)
-
-    return DummyEventTracker()
+    return EventTracker(name)
 
 
 if __name__ == '__main__':
     test_data = {'data': 'data'}
-    l = get_event_tracker(name='analytics')
-    l.track_event(routing_key='test.test', data=test_data)
-    while True:
-        pass
+    et = get_event_tracker(name='test')
+    et.track_event(routing_key='test.test', data=test_data)
+    et.track_event(routing_key='test.test.another', data=test_data)
--- stood/logger/logger.py
+++ stood/logger/logger.py
@@ -1,112 +1,49 @@
 import os
 import logging
-import queue
 import sys
-import time
 import uuid
 import datetime
 import traceback
-from threading import Thread
-from logging import StreamHandler
+from logging import StreamHandler, Formatter
+from logging.handlers import RotatingFileHandler
 from stood.config import config
-from elasticsearch import Elasticsearch
-import elasticsearch.helpers as helpers
+import json
 
 hostname = os.getenv('HOSTNAME', 'NO_HOSTNAME')
-
-MAPPINGS = """
-{
-  "mappings": {
-    "_doc": {
-      "properties": {
-        "timestamp": {
-          "type": "date"
-        }
-      }
-    }
-  }
-}
-"""
 
 
 def get_current_day():
     return datetime.datetime.today().strftime('%Y-%m-%d')
 
 
-class ElasticsearchHandler(StreamHandler):
-    def __init__(self):
-        super().__init__()
-        self.es = None
-        self.queue = queue.Queue()
-        setup_thread = Thread(target=self.setup)
-        setup_thread.start()
+class EsFormatter(Formatter):
+    def __init__(self, *args):
+        super().__init__(*args)
 
-    def setup(self):
-        try:
-            self.es = Elasticsearch(
-                    [config.ELASTICSEARCH_URL],
-                    sniff_on_start=True,
-                    sniff_on_connectoin_fail=True,
-                    snifffer_timeout=30
-                    )
-            loop_thread = Thread(target=self._loop)
-            loop_thread.start()
-        except Exception:
-            traceback.print_exc()
-            time.sleep(15)
-            self.setup()
+    def format(self, record):
+        new_day = get_current_day()
+        index = 'log-{}'.format(new_day)
 
-    def _loop(self):
-        while True:
-            time.sleep(10)
-            self._send_logs()
+        es_record = {
+                '_index': index,
+                '_type': '_doc',
+                '_id': str(uuid.uuid4()),
+                'file': record.pathname,
+                'line': record.lineno,
+                'msg': record.msg,
+                'level': record.levelname,
+                'timestamp': int(record.created * 1000),
+                'service': record.name,
+                'func': record.funcName,
+                'hostname': hostname
+                }
 
-    def add_record(self, record):
-        self.queue.put(record)
+        if record.exc_info:
+            es_record['exception'] = ''.join(traceback.format_exception(*record.exc_info))
 
-    def _send_logs(self):
-        try:
-            new_day = get_current_day()
-            index = 'log-{}'.format(new_day)
-            if not self.es.indices.exists(index=index):
-                self.es.indices.create(index, ignore=400, body=MAPPINGS)
-        except Exception:
-            traceback.print_exc()
-            return
+        json_string = json.dumps(es_record)
 
-        payload = []
-
-        try:
-            record = self.queue.get_nowait()
-        except queue.Empty:
-            record = None
-        while record is not None:
-            new_record = {
-                    '_index': index,
-                    '_type': '_doc',
-                    '_id': uuid.uuid4(),
-                    'file': record.pathname,
-                    'line': record.lineno,
-                    'msg': record.message,
-                    'level': record.levelname,
-                    'timestamp': int(record.created * 1000),
-                    'service': record.name,
-                    'func': record.funcName,
-                    'hostname': hostname
-                    }
-            if record.exc_info:
-                new_record['exception'] = ''.join(traceback.format_exception(*record.exc_info))
-            payload.append(new_record)
-            try:
-                record = self.queue.get_nowait()
-            except queue.Empty:
-                record = None
-
-        if self.es is None:
-            print('ERROR: dropped logs')
-            return
-
-        helpers.bulk(self.es, payload)
+        return json_string
 
     def emit(self, record):
         self.add_record(record)
@@ -124,12 +61,15 @@ def get_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(config.LEVEL)
 
-    if config.ELASTICSEARCH_ENABLED:
-        elasticsearch_handler = ElasticsearchHandler()
-        logger.addHandler(elasticsearch_handler)
+    elasticsearch_handler = RotatingFileHandler(
+            '/var/log/stood/{}.log'.format(name), maxBytes=1024 * 100, backupCount=5)
+    es_formatter = EsFormatter()
+    elasticsearch_handler.setFormatter(es_formatter)
+    logger.addHandler(elasticsearch_handler)
 
     stream_handler = StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(filename)s(%(lineno)d):%(funcName)s %(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter(
+            '%(filename)s(%(lineno)d):%(funcName)s %(asctime)s - %(name)s - %(levelname)s - %(message)s')
     stream_handler.setFormatter(formatter)
 
     logger.addHandler(stream_handler)
@@ -138,13 +78,12 @@ def get_logger(name):
 
 
 if __name__ == '__main__':
-    l = get_logger(name='test')
-    l.info('hello')
-    l.info('world')
-    l.error('uh oh')
+    log = get_logger(name='test')
+    log.info('hello')
+    log.info('world')
+    log.error('uh oh')
+
     try:
         raise Exception('test exception')
     except Exception:
-        l.exception('exception thrown')
-    while True:
-        pass
+        log.exception('exception thrown')
